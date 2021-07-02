439028aeb7fdbba8eedb8d56c7d180b00b107b4a
Dario Nakazima <dnakazima@hotmail.com>
2021-07-02 18:27:02 -0300

    aplicação generica

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,27 +5,32 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import entities.Product;
 import services.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
 
-		List<Integer> list = new ArrayList<>();
-
-		String path = "C:\\javatexto\\in.txt"; //valores  5 10 7 14 8
+		String path = "C:\\javatexto\\in.txt"; //valores  5 10 7 14 8 depois csv
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			String line = br.readLine();
 			while (line != null) {
-				list.add(Integer.parseInt(line));
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
 				line = br.readLine();
 			}
 
-			Integer x = CalculationService.max(list);
-			System.out.println("Max:");
+			Product x = CalculationService.max(list);//encontra o maior elemento da lista
+			System.out.println("Mais caro:");
 			System.out.println(x);
 
 		} catch (IOException e) {
--- src/services/CalculationService.java
+++ src/services/CalculationService.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class CalculationService {
 
-	public static Integer max(List<Integer> list) {
-		if (list.isEmpty()) {
+	public static <T extends Comparable<T>> T max(List<T> list) {//acha o maior numa lista de inteiros
+		if (list.isEmpty()) {//programação defensiva ver se não é vazia
 			throw new IllegalStateException("List can't be empty");
 		}
-		Integer max = list.get(0);
-		for (Integer item : list) {
+		T max = list.get(0);
+		for (T item : list) {
 			if (item.compareTo(max) > 0) {
 				max = item;
 			}
